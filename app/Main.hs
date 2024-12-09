9275a63487023f6428987275dc810881b59d3352
Rodrigo Villagomez <37837157+rod007vrr@users.noreply.github.com>
2024-12-09 13:28:12 -0500

    infinite run

--- app/Main.hs
+++ app/Main.hs
@@ -3,16 +3,20 @@
 module Main (main) where
 
 import CPUCollector (getCommandOutput, processListP)
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever)
 import Parser (Parser (..))
 import Parser qualified as P
 
 -- Parse /proc/stat to extract CPU times
 
 main :: IO ()
-main = do
+main = forever $ do
   content <- getCommandOutput
-  putStrLn "Debug output:" -- Add this debug line
+  putStrLn "Debug output:"
   putStrLn content
   case P.parse processListP content of
-    Right processes -> mapM_ print processes -- Print each process
+    Right processes -> mapM_ print processes
     Left err -> putStrLn $ "Error: " ++ err
+
+  threadDelay 1000000 -- 1 second
