ff21119ab2faca2b3d80c3542c2a7251eae47037
Rodrigo Villagomez <37837157+rod007vrr@users.noreply.github.com>
2024-12-16 16:06:08 -0500

    build issues fixed

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,6 @@ module Main (main) where
 
 import Control.Concurrent (threadDelay)
 import Control.Monad (forever)
-import Parser (Parser (..))
-import Parser qualified as P
 import ProcessCollector (getProcessList, printProcessList, processListP)
 
 main :: IO ()
